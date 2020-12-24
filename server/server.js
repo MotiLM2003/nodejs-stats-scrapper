026b66c02c5dead0d8aca2fb02db2e5c4e948cd3
Moti Elmakies <motiphone2003@gmail.com>
2020-12-24 23:13:33 +0200

    test 1

---
+++ server/server.js
@@ -0,0 +1,75 @@
+const path = require('path');
+const express = require('express');
+const request = require('request');
+const bodyParser = require('body-parser');
+const cheerio = require('cheerio');
+const cors = require('cors');
+const app = express();
+const PORT = process.env.PORT || 3000;
+const publicPath = path.join(__dirname, '..', 'build');
+
+console.log(publicPath);
+// app.use(cors());
+app.use(express.static(publicPath)).use(bodyParser.json());
+
+app.get('/api/data', (req, res) => {
+  console.log('api/data');
+  getData(res);
+});
+
+if (process.env.NODE_ENV === 'production') {
+}
+app.get('*', (req, res) => {
+  const fullPath = path.join(publicPath, 'index.html');
+  console.log('sending file:', fullPath);
+  res.sendFile(fullPath);
+});
+
+app.listen(PORT, () => {
+  console.log('server is up!');
+});
+
+const getData = (res) => {
+  const data = {
+    wins: 0,
+    level: 0,
+    kd: 0,
+    kills: 0,
+  };
+  request(
+    'https://cod.tracker.gg/warzone/profile/atvi/DAMAGEX%235551439/overview',
+    (error, response, html) => {
+      if (!error && response.statusCode === 200) {
+        console.log('in');
+        const $ = cheerio.load(html);
+
+        data.level = parseInt($('.highlight-text').text().split(' ')[7]);
+
+        const value = $('.numbers .value');
+        $('.numbers .value').each((i, elm) => {
+          switch (i) {
+            case 0: {
+              data.wins = elm.children[0].data;
+              break;
+            }
+            case 2: {
+              data.kd = elm.children[0].data;
+              break;
+            }
+            case 6: {
+              data.kills = elm.children[0].data;
+              break;
+            }
+          }
+          // console.log(elm.children[0].data);
+        });
+
+        console.log('clg data', data);
+        res.json(data);
+      } else {
+        console.log(response.statusCode);
+        res.send('error', resposne);
+      }
+    }
+  );
+};
---
+++ src/components/App.js
@@ -0,0 +1,40 @@
+import React, { useState, useEffect } from 'react';
+import api from '../api';
+import axios from 'axios';
+
+import image from '../images/background.JPG';
+import '../style.css';
+const App = () => {
+  const [stats, setStats] = useState({
+    wins: 0,
+    level: 0,
+    kd: 0,
+    kills: 0,
+  });
+
+  useEffect(() => {
+    // const interval = setInterval(() => {
+    //   getData();
+    // }, 125000);
+    // getData();
+  }, []);
+
+  const getData = async () => {
+    const { data } = await axios.get('api/data');
+    setStats(data);
+    console.log(data);
+  };
+
+  return (
+    <div className='container'>
+      <img className='img' src={image} alt='statistics' />
+      <p className='wins'>{stats.wins}</p>
+      <p className='level'>{stats.level}</p>
+      <p className='kd'>{stats.kd}</p>
+      <p className='kills'>{stats.kills}</p>
+      <button onClick={() => getData()}>Hello</button>
+    </div>
+  );
+};
+
+export default App;
